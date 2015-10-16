3ae6c05360cc7cf2741b74305689fa4cef97f7cf
wowgs <wowgss@gmail.com>
2015-10-16 11:49:22 +0300

    Update thirdBits.c

--- task2/thirdBits.c
+++ task2/thirdBits.c
@@ -1,8 +1,8 @@
 int thirdBits (void)
 {
-    int x=146;
-    x=(x<<9)+146;
-    x=(x<<9)+146;
-    x=(x<<4)+4;
+    int x = 146;
+    x = (x << 9) + 146;
+    x = (x << 9) + 146;
+    x = (x << 4) + 4;
     return x;
 }
