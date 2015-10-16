66f44ee71ab27445eb2aac6dd20de54b9bdb4878
wowgs <wowgss@gmail.com>
2015-10-16 11:47:56 +0300

    Update logicalShift.c

--- task2/logicalShift.c
+++ task2/logicalShift.c
@@ -1,6 +1,6 @@
-int logicalShift(int x,int n)
+int logicalShift(int x, int n)
 {
-    int q=x>>n;
-    int z=~((1<<31)>>n+~0);
-    return z&q;
+    int q = x >> n;
+    int z = ~((1 << 31) >> n + ~0);
+    return z & q;
 }
