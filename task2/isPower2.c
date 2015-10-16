b7c76906c99eba311f10f79c59b9147ab756874c
wowgs <wowgss@gmail.com>
2015-10-16 11:47:24 +0300

    Update isPower2.c

--- task2/isPower2.c
+++ task2/isPower2.c
@@ -1,8 +1,8 @@
 int isPower2(int x)
 {
-    int a=!!x;
-    int b=!((x>>31)&1);
-    int q=~(x)+1;
-    int c=!((q|x)+~(~x+1)+1);
-    return a&b&c;
+    int a = !!x;
+    int b = !((x >> 31) & 1);
+    int q = ~(x) + 1;
+    int c = !((q | x) + ~(~x + 1) + 1);
+    return a & b & c;
 }
