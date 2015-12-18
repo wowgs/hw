a42efe8c34138178961d2058c3525105c32f547f
Alexander Minayev <wowgss@gmail.com>
2015-12-18 13:30:16 +0300

    new hash

--- strings/2.c
+++ strings/2.c
@@ -1,11 +1,15 @@
+#define hash_const 31
+#define size_of_arr 100003
+#define max_str_size 256
+
 unsigned int bin(int q)
 {
     if (q == 0) return 1;
-    if (q == 1) return 31;
+    if (q == 1) return hash_const;
     if (q % 2 == 1)
     {
         int j = bin(q / 2);
-        return j * j * 31;
+        return j * j * hash_const;
     }
     int j = bin(q / 2);
     return j * j;
@@ -17,7 +21,7 @@ int hash (char* s)
     int x = 0;
     while (*(s + x) != '\0')
     {
-        q = (q + (bin(x) * (int)(*s + x)) % 100003) % 100003;
+        q = (q + (bin(x) * (int)(*s + x)) % size_of_arr) % size_of_arr;
         x++;
     }
     return q;
@@ -28,13 +32,14 @@ void penta()
     int n;
     printf("Type number of strings\n");
     scanf("%d", &n);
-    int ar[100003];
+    printf("Type strings:\n");
+    int ar[size_of_arr];
     int h[n];
-    char st[n][256];
+    char st[n][max_str_size];
     char q;
     scanf("%c", &q);
     int x, y, i;
-    for (x = 0; x < 100003; x++)
+    for (x = 0; x < size_of_arr; x++)
     {
         ar[x] = 0;
     }
@@ -54,6 +59,7 @@ void penta()
     }
     i = 0;
     q = st[0][i];
+    printf("The answer is:\n");
     for (x = 0; x < n; x++)
     {
         if (ar[h[x]] > 0)
@@ -71,4 +77,10 @@ void penta()
         }
     }
     return;
+}
+
+int main()
+{
+    penta();
+    return 0;
 }
